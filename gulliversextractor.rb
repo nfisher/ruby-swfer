ef0edb2d3e65f59761dc33e06b1b6823005079fb
Nathan Fisher <nfisher@junctionbox.ca>
2008-09-03 09:39:31 -0400

    gulliversextractor.rb:
    	* Remove all file dependencies.
    
    swfreader.rb:
    
    	* Add get_bytes function to retrieve a specified number of bytes from the associated file.

--- gulliversextractor.rb
+++ gulliversextractor.rb
@@ -1,13 +1,5 @@
 class GulliversExtractor
-	BYTE = 1
-	WORD = 2
-	DWORD = 4
-	QWORD = 8
-
-	def initialize( file )
-		@file = file
-	end
-
+	
 	# Decodes a 64-bit unsigned integer in big endian format.
 	#
 	def big_ui64
@@ -17,14 +9,14 @@ class GulliversExtractor
 	# Decodes a 32-bit unsigned integer in big endian format.
 	#
 	def big_ui32( tmp = nil )
-		read_bytes(DWORD,'N',tmp)
+		convert_bytes('N',tmp)
 	end
 
 
 	# Decodes a 16-bit unsigned integer in big endian format.
 	#
 	def big_ui16( tmp = nil )
-		read_bytes(WORD,'n',tmp)
+		convert_bytes('n',tmp)
 	end
 	
 
@@ -37,33 +29,25 @@ class GulliversExtractor
 	# Decodes a 32-bit unsigned integer in little endian format.
 	#
 	def little_ui32( tmp = nil )
-		read_bytes(DWORD,'V',tmp)
+		convert_bytes('V',tmp)
 	end
 
 
 	# Decodes a 16-bit unsigned integer in little endian format.
 	#
 	def little_ui16( tmp = nil )
-		read_bytes(WORD,'v',tmp)
+		convert_bytes('v',tmp)
 	end
 
 
 	# Decodes a single unsigned byte.
 	#
 	def ui8( tmp = nil )
-		read_bytes(BYTE,'C',tmp)
-	end
-
-
-	# Retrieves a string the length specified.
-	#
-	def gets( len )
-		@file.read( len )
+		convert_bytes('C',tmp)
 	end
 
 	private
-	def read_bytes( count, type, tmp = nil )
-		tmp = @file.read( count ) if tmp.nil?
+	def convert_bytes( type, tmp = nil )
 		tmp.unpack( type ).shift
 	end
 end
