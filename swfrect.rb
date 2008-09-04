6497dbecb19d488ef76462f2b2d63f2dc381de94
Nathan Fisher <nfisher@junctionbox.ca>
2008-09-04 09:42:49 -0400

    swfrect.rb:
    	* Modified bytes to shift each byte in place for the private @bytes member.
    	* Modified private method at to locate a specific set of bits in the range of bytes.

--- swfrect.rb
+++ swfrect.rb
@@ -4,26 +4,58 @@ class SwfRect
 	attr :max_x, true
 	attr :max_y, true
 
-	attr :nbits
+	def initialize
+		@nbits = nil
+	end
 
+	def required_bytes
+		return nil if @nbits.nil?	
 
-	def required_bytes( ub )
+		@required_bytes = ( (@nbits * 4 + 5) / 8.0 ).ceil
+	end
+
+	def nbits
+		@nbits
+	end
+
+	def nbits=( byte )
+		ub = byte
 		ub &= 0b11111000
 		@nbits = ub >> 3
-
-		( (@nbits * 4 + 5) / 8.0 ).ceil
 	end
 
 	def bytes=( bytes )
-		@bytes = []
+		@bytes = 0
 		bytes.each_byte do |ch|
-			@bytes << ch
+			@bytes |= ch
+			@bytes = @bytes << 8
 		end
+
+		@min_x = at( 0 )
+		puts "min x: #{@min_x}"
+
+		@max_x = at( 1 )
+		puts "max x: #{@max_x}"
+
+		@min_y = at( 2 )
+		puts "min y: #{@min_y}"
+
+		@max_y = at( 3 )
+		puts "max y: #{@max_y}"
 	end
 
 	private
 	def at( i )
-		
+		value = 0
+		first =	5 + (i * @nbits) 
+		last = first + @nbits - 1
+
+		(first..last).each do |i|
+			value |= @bytes
+			value = value << 1 unless i == last
+		end
+
+		value
 	end
 end
 
--- swfrecttest.rb
+++ swfrecttest.rb
@@ -12,34 +12,50 @@ class SwfRectTest < Test::Unit::TestCase
 		@rect = nil
 	end
 
-	def test_required_bytes_for_1bit_datapack
+	def test_required_bytes_with_bytes_not_set
+		assert_equal( nil, @rect.required_bytes )
+	end
+
+	def test_nbits_for_1bit_datapack
 		val = 0b00001000
-		assert_equal( 2, @rect.required_bytes(val) )
+		@rect.nbits = val
+		assert_equal( 1, @rect.nbits )
 	end
 
-	def test_required_bytes_for_2bit_datapack 
+	def test_nbits_for_2bit_datapack 
 		val = 0b00010010
-		assert_equal( 2, @rect.required_bytes(val) )
+		@rect.nbits = val
+		assert_equal( 2, @rect.nbits )
 	end
 
-	def test_required_bytes_for_16bit_datapack
+	def test_nbits_for_16bit_datapack
 		val = 0b10000000
-		assert_equal( 9, @rect.required_bytes(val) )
+		@rect.nbits = val
+		assert_equal( 16, @rect.nbits )
 	end
 
-	def test_required_bytes_with_all_bits_on
+	def test_nbits_with_all_bits_on
 		val = 0b11111111
-		assert_equal( 17, @rect.required_bytes(val) )
+		@rect.nbits = val
+		assert_equal( 31, @rect.nbits )
 	end
 
-	def test_required_bytes_with_all_bits_off
+	def test_nbits_with_all_bits_off
 		val = 0
-		assert_equal( 1, @rect.required_bytes(val) )
+		@rect.nbits = val
+		assert_equal( 0, @rect.nbits )
 	end
 
-	def test_default_frame_byte
+	def test_default_frame_nbits
 		val = 0b01111000
-		@rect.required_bytes( val )
+		@rect.nbits = val
 		assert_equal( 15, @rect.nbits )
+	end
+
+	def test_default_frame
+		val = 0b01111000000000000000011111010000000000000000000000011111010000000
+		last = val.size * 8
+		# @rect.nbits = val[0]
+		# assert_equal( 15, @rect.nbits )
 	end
 end
