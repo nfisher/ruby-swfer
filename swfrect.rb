a9ae150e975e06284d679e926fdb247f5cb91196
Nathan Fisher <nfisher@junctionbox.ca>
2008-09-09 21:56:49 -0400

    swfrect.rb:
    	* Modified at to use an initial_offset which accounts for BigNums DWORD boundaries.
    
    swfrecttest.rb:
    
    	* Modified test_default_frame to use a byte pack from a 700 x 500 SWF header.

--- swfrect.rb
+++ swfrect.rb
@@ -1,4 +1,6 @@
 class SwfRect
+	NBITS_FIELD_LENGTH = 5
+	BYTE_SIZE = 8
 	attr :min_x, true
 	attr :min_y, true
 	attr :max_x, true
@@ -24,36 +26,51 @@ class SwfRect
 		@nbits = ub >> 3
 	end
 
+
+	#
+	# bytes=: extracts the min and max for x and y.
+	#
 	def bytes=( bytes )
 		@bytes = 0
-		bytes.each_byte do |ch|
-			@bytes |= ch
-			@bytes = @bytes << 8
+
+		bits = bytes.unpack( "B*" ).shift
+
+		bits.each_byte do |bit|
+			@bytes |= 1 if bit == 49
+			@bytes << 1
 		end
+		puts @bytes
 
 		@min_x = at( 0 )
 		puts "min x: #{@min_x}"
 
 		@max_x = at( 1 )
-		puts "max x: #{@max_x}"
+		puts "max x: #{@max_x/20}"
 
 		@min_y = at( 2 )
 		puts "min y: #{@min_y}"
 
 		@max_y = at( 3 )
-		puts "max y: #{@max_y}"
+		puts "max y: #{@max_y/20}"
 	end
 
 	private
 	def at( i )
 		value = 0
-		first =	5 + (i * @nbits) 
-		last = first + @nbits - 1
+		# Bignum appears to buffer on 4 byte boundaries so 9 bytes is padded with
+		# zeros to 12 bytes
+		initial_offset = (@bytes.size - required_bytes) * BYTE_SIZE + NBITS_FIELD_LENGTH
+		first =	initial_offset + ( i * @nbits ) 
+		last = first + @nbits
 
-		(first..last).each do |i|
-			value |= @bytes
-			value = value << 1 unless i == last
+		sign_bit = @bytes[first]
+
+		(first...last).each do |i|
+			value |= @bytes[i]
+			value = value << 1
 		end
+
+		value *= -1 if sign_bit == 1
 
 		value
 	end
